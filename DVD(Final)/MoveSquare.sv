cfe224e0195a53128a8e1824854c4bb545c7bfba
SeanDauch <seandauch1@gmail.com>
2026-06-05 23:30:57 -0700

    Added header

--- DVD(Final)/MoveSquare.sv
+++ DVD(Final)/MoveSquare.sv
@@ -1,3 +1,8 @@
+// ============================================================================
+// Project Name: MoveSquare
+// Author:       Charles Aebi, Sean Dauch
+// Description:  Provide physics for the movement of the box
+// ============================================================================
 module MoveSquare#(
 	parameter SPEEDBITS = 5
 )(
@@ -16,6 +21,8 @@ module MoveSquare#(
 );
 
 always_ff @(posedge clock) begin
+
+	// initial position for the box
 	if (reset) begin 
 		upper <= 10'd239;
 		lower <= 10'd358;
@@ -23,21 +30,21 @@ always_ff @(posedge clock) begin
 		right <= 10'd679;
 	end
 	else if (enable) begin 
-		// check if the upper is above pixel 1
-		if (direction_up) begin 
+
+		if (direction_up) begin // move box up
 			upper <= upper - speed_y;
 			lower <= lower - speed_y;
 		end
-		else if (!direction_up) begin 
+		else if (!direction_up) begin // move box down
 			upper <= upper + speed_y;
 			lower <= lower + speed_y;
 		end
 	
-		if (direction_left) begin
+		if (direction_left) begin // move box left
 			left <= left - speed_x;
 			right <= right - speed_x;
 		end
-		else if (!direction_left) begin
+		else if (!direction_left) begin // move box right
 			left <= left + speed_x;
 			right <= right + speed_x;
 		end
