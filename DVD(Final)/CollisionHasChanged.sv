49836fa718ace563628d4b30d177becfe95f318e
SeanDauch <seandauch1@gmail.com>
2026-06-05 22:22:04 -0700

    Added Header

--- DVD(Final)/CollisionHasChanged.sv
+++ DVD(Final)/CollisionHasChanged.sv
@@ -1,3 +1,9 @@
+// ============================================================================
+// Project Name: CollisionHasChanged
+// Author:       Charles Aebi
+// Description:  Checks if either collision_h or collision_v has changed
+// ============================================================================
+
 module CollisionHasChanged(
 	input logic clock50,
 	input logic reset,
@@ -17,6 +23,8 @@ always_ff @(posedge clock50) begin
 	else begin
       prev_collision_h      <= collision_h;
       prev_collision_v      <= collision_v;
+
+		// collsion has changed is high if either collision doesnt equal the respective previous collision	
       collision_has_changed <= (collision_h != prev_collision_h) || (collision_v != prev_collision_v);
    end
 end
